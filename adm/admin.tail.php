2640811fa7906459375de14c20de22431eac2e45
1clickmake <hades708@naver.com>
2025-07-12 15:33:09 +0900

    First commit for cm_board_7 project

--- adm/admin.tail.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (!defined('_CMBOARD_')) exit; // 개별 페이지 접근 불가
-?>
-
-
-<?php
-include_once CM_PATH.'/tail_sub.php';
-?>
--- board/write_update_spam_filter.php
+++
@@ -1,6 +0,0 @@
-<?php
-include_once './_common.php';
-
-		
-
-      
